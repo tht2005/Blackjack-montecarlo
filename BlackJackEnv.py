50b2ef958bd79042aede063d1353a3f4e5cfd08f
Dang Duong <dangduong31205@gmail.com>
2024-04-23 18:36:35 +0700

    update Env

--- BlackJackEnv.py
+++ BlackJackEnv.py
@@ -1,4 +1,18 @@
+import numpy as np
+
 class Env:
-    def __init__(self):
-        pass
+    def initEpisode(self):
+        self.cur = 0
+        self.values = []
+        for i in range(1, 13 + 1):
+            self.values.append(i)
+            self.values.append(i)
+            self.values.append(i)
+            self.values.append(i)
+        np.random.shuffle(self.values)
+
+    def hit(self):
+        ret = self.values[self.cur]
+        self.cur += 1
+        return ret
 
