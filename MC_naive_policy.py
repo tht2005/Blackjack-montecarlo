dc811af8df8aa4256c26d906ed2a75931e6b7f02
Dang Duong <dangduong31205@gmail.com>
2024-04-23 18:37:15 +0700

    update MC naive

--- MC_naive_policy.py
+++ MC_naive_policy.py
@@ -45,10 +45,6 @@ for epi in range(NEPISODE):
     S = sum(init_card)
     has_ace = 1 in init_card
 
-    usable_ace = int(has_ace and S <= 11)
-    if usable_ace:
-        S += 10
-
     if has_ace and S == 11: # natural case
         update([1, dealer_faceup, S], Q, N, 1)
         continue
@@ -57,12 +53,17 @@ for epi in range(NEPISODE):
     bust = False
     STATES = [ ]
     while True:
-        STATES.append([ int(usable_ace), dealer_faceup, S ])
-        if S == 20 or S == 21:
+        usable_ace = has_ace and S <= 11
+        curS = S + (10 if usable_ace else 0)
+        STATES.append([ int(usable_ace), dealer_faceup, curS ])
+
+        if curS == 20 or curS == 21:
             break
 
         draw = np.minimum(10, env.hit())
         S += draw
+        if draw == 1:
+            has_ace = True
 
         if S > 21: # go bust
             bust = True
@@ -73,9 +74,23 @@ for epi in range(NEPISODE):
     if bust:
         R = -1
     else:
-        while dealer_sum < 17:
+        if has_ace and S <= 11:
+            S += 10
+
+        while True:
+            dealer_cur_sum = dealer_sum + (10 if (dealer_has_ace and dealer_sum <= 11) else 0)
+
+            if dealer_cur_sum >= 17:
+                break
+
             draw = np.minimum(10, env.hit())
             dealer_sum += draw
+            if draw == 1:
+                dealer_has_ace = True
+
+        if dealer_has_ace and dealer_sum <= 11:
+            dealer_sum += 10
+
         if dealer_sum > 21 or dealer_sum < S:
             R = 1
         elif dealer_sum > S:
@@ -98,12 +113,19 @@ fig, axis = plt.subplots(nrows=2, ncols=2)
 counter = 0
 for ax in np.reshape(axis, newshape=2*2):
     im = ax.imshow(PLOT_DATA[counter], vmin=-1, vmax=1)
+    ax.set_xticks([])
+    ax.set_yticks([])
     counter += 1
 
-axis[0][0].set_title('Unusable Ace', size='large')
-axis[0][1].set_title('Usable Ace', size='large')
-axis[0][0].set_ylabel('After {} episodes'.format(TRACKED_EPISODES[0]), size='large')
-axis[1][0].set_ylabel('After {} episodes'.format(TRACKED_EPISODES[1]), size='large')
+axis[1][1].set_xticks([0, 9], ['12', '21'])
+axis[1][1].set_yticks([0, 9], ['A', '10'])
+axis[1][1].set_xlabel('Tổng điểm')
+axis[1][1].set_ylabel('Bài ngửa của nhà cái')
+
+axis[0][0].set_title('Coi quân Át 1 điểm', size='large')
+axis[0][1].set_title('Coi quân Át 11 điểm', size='large')
+axis[0][0].set_ylabel('Sau {} episode'.format(TRACKED_EPISODES[0]), size='large')
+axis[1][0].set_ylabel('Sau {} episodes'.format(TRACKED_EPISODES[1]), size='large')
 
 fig.subplots_adjust(right=0.95)
 cbar_ax = fig.add_axes([0.87, 0.15, 0.03, 0.7])
